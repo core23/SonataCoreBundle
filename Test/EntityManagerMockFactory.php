dd6ed5b56c44cda30cff27d0a6a461b3a0fb7c4e
core23 <mail@core23.de>
2016-06-04 13:17:56 +0200

    Removed deprecated phpunit methods

--- Test/EntityManagerMockFactory.php
+++ Test/EntityManagerMockFactory.php
@@ -25,11 +25,11 @@ class EntityManagerMockFactory
      */
     public static function create(\PHPUnit_Framework_TestCase $test, \Closure $qbCallback, $fields)
     {
-        $query = $test->getMockForAbstractClass('Doctrine\ORM\AbstractQuery', array(), '', false, true, true, array('execute'));
+        $query = $test->createMock('Doctrine\ORM\AbstractQuery', array(), '', false, true, true, array('execute'));
         $query->expects($test->any())->method('execute')->will($test->returnValue(true));
 
         if (Version::compare('2.5.0') < 1) {
-            $entityManager = $test->getMock('Doctrine\ORM\EntityManagerInterface');
+            $entityManager = $test->createMock('Doctrine\ORM\EntityManagerInterface');
             $qb = $test->getMockBuilder('Doctrine\ORM\QueryBuilder')->setConstructorArgs(array($entityManager))->getMock();
         } else {
             $qb = $test->getMockBuilder('Doctrine\ORM\QueryBuilder')->disableOriginalConstructor()->getMock();
@@ -47,7 +47,7 @@ class EntityManagerMockFactory
         $repository = $test->getMockBuilder('Doctrine\ORM\EntityRepository')->disableOriginalConstructor()->getMock();
         $repository->expects($test->any())->method('createQueryBuilder')->will($test->returnValue($qb));
 
-        $metadata = $test->getMock('Doctrine\Common\Persistence\Mapping\ClassMetadata');
+        $metadata = $test->createMock('Doctrine\Common\Persistence\Mapping\ClassMetadata');
         $metadata->expects($test->any())->method('getFieldNames')->will($test->returnValue($fields));
         $metadata->expects($test->any())->method('getName')->will($test->returnValue('className'));
 
